ebf66811192c0121d67619d2d695b99783d27248
Lily Sellers <lilygsellers@gmail.com>
2017-10-16 14:27:01 -0700

    Task and gen_tcp

--- apps/kv_server/mix.exs
+++ apps/kv_server/mix.exs
@@ -2,16 +2,18 @@ defmodule KVServer.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :kv_server,
-     version: "0.1.0",
-     build_path: "../../_build",
-     config_path: "../../config/config.exs",
-     deps_path: "../../deps",
-     lockfile: "../../mix.lock",
-     elixir: "~> 1.3",
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     deps: deps]
+    [
+      app: :kv_server,
+      version: "0.1.0",
+      build_path: "../../_build",
+      config_path: "../../config/config.exs",
+      deps_path: "../../deps",
+      lockfile: "../../mix.lock",
+      elixir: "~> 1.3",
+      build_embedded: Mix.env == :prod,
+      start_permanent: Mix.env == :prod,
+      deps: deps()
+    ]
   end
 
   # Configuration for the OTP application
